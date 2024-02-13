b73a3759b834c5ff11a5d7a3cc510e54f414dec0
oscarborras <oscarborras@iesjulioverne.es>
2024-02-13 14:48:52 +0100

    mejoras visuales

--- src/app/ejngmodel/ejngmodel.component.ts
+++ src/app/ejngmodel/ejngmodel.component.ts
@@ -16,11 +16,11 @@ export class EjngmodelComponent {
     precio: 0
   }
 
-  articulos = [{ codigo: 1, descripcion: 'papas', precio: 10.55 },
-  { codigo: 2, descripcion: 'manzanas', precio: 12.10 },
-  { codigo: 3, descripcion: 'melon', precio: 52.30 },
-  { codigo: 4, descripcion: 'cebollas', precio: 17 },
-  { codigo: 5, descripcion: 'calabaza', precio: 20 },
+  articulos = [ { codigo: 1, descripcion: 'papas', precio: 10.55 },
+                { codigo: 2, descripcion: 'manzanas', precio: 12.10 },
+                { codigo: 3, descripcion: 'melon', precio: 52.30 },
+                { codigo: 4, descripcion: 'cebollas', precio: 17 },
+                { codigo: 5, descripcion: 'calabaza', precio: 20 },
   ];
 
   hayRegistros() {
